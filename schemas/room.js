f911e603b3cd7c8cc6fc6901a69feae50af84447
Qli <coollooc02@naver.com>
2025-03-25 17:01:54 +0900

    original files

--- schemas/room.js
+++ schemas/room.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
@@ -40,10 +41,5 @@ const roomSchema = new Schema({
   toObject: { virtuals: true },
   timestamps: true
 });
-
-// 성능 최적화를 위한 인덱스 추가
-roomSchema.index({ owner: 1 });
-roomSchema.index({ participants: 1 });
-roomSchema.index({ channelId: 1 }, { unique: true });
 
 module.exports = mongoose.model('Room', roomSchema);
